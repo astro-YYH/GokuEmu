b46307173db3b711b17d7f33de6f550392de196c
Yanhui Yang <yanhui.yang@email.ucr.edu>
2024-08-22 14:12:53 -0500

    narrow pre test

--- emulator/emu_submit_small.sh
+++ emulator/emu_submit_small.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-#SBATCH --job-name=emu-EE2
-#SBATCH --time=0-48:00:00
+#SBATCH --job-name=narrow-pre
+#SBATCH --time=0-12:00:00
 # SBATCH --mem=128G
 #SBATCH --nodes=2   # no more than num redshifts
-#SBATCH --ntasks-per-node=1
+#SBATCH --ntasks-per-node=3
 #SBATCH --partition=small
 #SBATCH -A AST21005
 
+source ~/.bashrc
+conda activate gpy-env
+
 hostname
 date
-echo '--job-name=emu-EE2'
-ibrun python -u matter_pow_emu.py --outdir EE2_compare
+echo '--job-name=narrow'
+ibrun python -u matter_pow_emu_narrow_pre.py --outdir narrow_pre_test
 date
 
