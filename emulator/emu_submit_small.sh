263114c31f5003efe8700d240168403a1602e6ea
Yanhui Yang <yanhui.yang@email.ucr.edu>
2024-09-06 21:28:06 -0500

    pre combined prediction

--- emulator/emu_submit_small.sh
+++ emulator/emu_submit_small.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --job-name=narrow-pre
-#SBATCH --time=0-12:00:00
+#SBATCH --job-name=pre-combined
+#SBATCH --time=0-24:00:00
 # SBATCH --mem=128G
-#SBATCH --nodes=2   # no more than num redshifts
-#SBATCH --ntasks-per-node=3
+#SBATCH --nodes=1   # no more than num redshifts
+#SBATCH --ntasks-per-node=1
 #SBATCH --partition=small
 #SBATCH -A AST21005
 
@@ -12,7 +12,9 @@ conda activate gpy-env
 
 hostname
 date
-echo '--job-name=narrow'
-ibrun python -u matter_pow_emu_narrow_pre.py --outdir narrow_pre_test
+echo '--job-name=pre-combined'
+# ibrun python -u matter_pow_emu_narrow_pre.py --outdir narrow_pre_test
+# matter_pow_emu_pre_combined-W-N.py
+ibrun python -u matter_pow_emu_pre_combined-W-N.py --outdir pre_combined_test
 date
 
