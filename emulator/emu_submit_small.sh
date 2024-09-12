06b01bfc64e708a98e29617f30dd167b974a2c5a
Yanhui Yang <yanhui.yang@email.ucr.edu>
2024-09-11 22:38:20 -0500

    frontera wide pre test

--- emulator/emu_submit_small.sh
+++ emulator/emu_submit_small.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=pre-combined
+#SBATCH --job-name=pre-wide
 #SBATCH --time=0-24:00:00
 # SBATCH --mem=128G
 #SBATCH --nodes=2   # no more than num redshifts
@@ -12,9 +12,10 @@ conda activate gpy-env
 
 hostname
 date
-echo '--job-name=pre-combined'
+echo '--job-name=wide-pre-test'
 # ibrun python -u matter_pow_emu_narrow_pre.py --outdir narrow_pre_test
 # matter_pow_emu_pre_combined-W-N.py
-ibrun python -u matter_pow_emu_pre_combined-W-N.py --outdir pre_combined_test
+# ibrun python -u matter_pow_emu_pre_combined-W-N.py --outdir pre_combined_test
+ibrun python -u matter_pow_emu_wide_pre.py --outdir wide_pre_test_frontera
 date
 
