a418ecea30afd263bc1706787b3b7e08fe4a3795
yyang440 <yyang440@login1.vista.tacc.utexas.edu>
2025-01-30 16:16:24 -0600

    w and n trained

--- emulator/emu_submit.sh
+++ emulator/emu_submit.sh
@@ -15,6 +15,8 @@ hostname
 date
 echo '--job-name=emu-train'
 # ibrun python -u matter_pow_emu.py --outdir Goku-W_single_param_dependence
-ibrun python -u matter_pow_emu_train.py --outdir pre-trained/goku  # GokuEmu
+# ibrun python -u matter_pow_emu_train.py --outdir pre-trained/goku  # GokuEmu
+ibrun python -u matter_pow_emu_train.py --L1HF_base ../data/narrow/matter_power_564_Box1000_Part750_15_Box1000_Part3000 --L2HF_base ../data/narrow/matter_power_564_Box250_Part750_15_Box1000_Part3000 --outdir pre-trained/goku-n  # GokuEmu-n
+ibrun python -u matter_pow_emu_train.py --L1HF_base ../data/matter_power_564_Box1000_Part750_21_Box1000_Part3000 --L2HF_base ../data/matter_power_564_Box250_Part750_21_Box1000_Part3000 --outdir pre-trained/goku-w  # GokuEmu-w
 date
 
